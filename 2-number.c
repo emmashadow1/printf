ceb986759d9c0dfdf31a3b0383aed91f09383364
root <oluruth15@gmail.com>
2022-10-18 10:23:12 -0700

    addition

--- 2-number.c
+++ 2-number.c
@@ -1,6 +1,15 @@
+#include "main.h"
+/**
+ * print_from_to - prints a range of char addresses
+ * @start: starting address
+ * @stop: stopping address
+ * @except: except address
+ * Return: number bytes printed
+ */
 int print_from_to(char *start, char *stop, char *except)
 {
-int sum = 0;
+
+	iint sum = 0;
 while (start <= stop)
 {
 if (start != except)
@@ -61,3 +70,4 @@ count += _putchar(a[i]);
 i++;
 }
 return (count);
+}
